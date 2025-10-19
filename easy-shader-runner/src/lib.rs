f0ff4abcd25a75ad65d01776212478c8e6195c92
Ross Younger <crazyscot@gmail.com>
2025-10-19 17:18:57 +1300

    add top-level error handling

--- easy-shader-runner/src/lib.rs
+++ easy-shader-runner/src/lib.rs
@@ -1,5 +1,6 @@
 use egui_winit::winit::event_loop::EventLoop;
 use std::borrow::Cow;
+use std::path::PathBuf;
 use user_event::CustomEvent;
 
 pub use context::GraphicsContext;
@@ -23,6 +24,26 @@ mod shader;
 mod ui;
 mod user_event;
 
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    EventLoopError(#[from] egui_winit::winit::error::EventLoopError),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error("Missing CARGO_MANIFEST_DIR")]
+    MissingCargoManifest,
+    #[error("Shader directory {0} not found")]
+    ShaderDirectoryNotFound(PathBuf),
+    #[cfg(all(
+        any(feature = "runtime-compilation", feature = "hot-reload-shader"),
+        not(target_arch = "wasm32")
+    ))]
+    #[error(transparent)]
+    BuildFailed(spirv_builder::SpirvBuilderError),
+    #[error("Build failed")]
+    BuildFailedQuietly,
+}
+
 /// Run with runtime compilation
 ///
 /// `shader_crate_path` is relative to CARGO_MANIFEST_DIR
@@ -36,7 +57,7 @@ pub fn run_with_runtime_compilation<C: ControllerTrait + Send>(
     shader_crate_path: impl AsRef<std::path::Path>,
     title: impl Into<String>,
 ) {
-    run_with_runtime_compilation_2(controller, shader_crate_path, title, true);
+    run_with_runtime_compilation_2(controller, shader_crate_path, title, true).unwrap();
 }
 
 /// Run with runtime compilation
@@ -53,17 +74,18 @@ pub fn run_with_runtime_compilation_2<C: ControllerTrait + Send>(
     shader_crate_path: impl AsRef<std::path::Path>,
     title: impl Into<String>,
     relative_to_manifest: bool,
-) {
+) -> Result<(), Error>
+{
     setup_logging();
-    let event_loop = EventLoop::with_user_event().build().unwrap();
+    let event_loop = EventLoop::with_user_event().build()?;
     // Build the shader before we pop open a window, since it might take a while.
     let shader_path = shader::compile_shader(
         #[cfg(feature = "hot-reload-shader")]
         event_loop.create_proxy(),
         shader_crate_path,
         relative_to_manifest,
-    );
-    let shader_bytes = std::fs::read(shader_path).unwrap();
+    )?;
+    let shader_bytes = std::fs::read(shader_path)?;
     start(event_loop, controller, shader_bytes, title.into())
 }
 
@@ -74,7 +96,17 @@ pub fn run_with_prebuilt_shader<C: ControllerTrait>(
 ) {
     setup_logging();
     let event_loop = EventLoop::with_user_event().build().unwrap();
-    start(event_loop, controller, shader_bytes, title.into());
+    start(event_loop, controller, shader_bytes, title.into()).unwrap();
+}
+
+pub fn run_with_prebuilt_shader_2<C: ControllerTrait>(
+    controller: C,
+    shader_bytes: &'static [u8],
+    title: impl Into<String>,
+) -> Result<(), Error> {
+    setup_logging();
+    let event_loop = EventLoop::with_user_event().build()?;
+    start(event_loop, controller, shader_bytes, title.into())
 }
 
 fn start<C: ControllerTrait>(
@@ -82,14 +114,14 @@ fn start<C: ControllerTrait>(
     controller: C,
     shader_bytes: impl Into<Cow<'static, [u8]>>,
     title: String,
-) {
+) -> Result<(), Error> {
     let mut app = app::App::new(
         event_loop.create_proxy(),
         shader_bytes.into(),
         controller,
         title,
     );
-    event_loop.run_app(&mut app).unwrap()
+    Ok(event_loop.run_app(&mut app)?)
 }
 
 pub fn setup_logging() {
